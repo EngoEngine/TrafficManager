387dcf6131abf714ce950a521fc4e3dc1fa6bce0
Etienne Bruines <etienne.bruines@huperwebs.nl>
2016-04-10 21:08:42 +0200

    Added initial code

---
+++ traffic.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"engo.io/engo"
+	"engo.io/ecs"
+	"image/color"
+)
+
+type myGame struct {}
+
+// Type uniquely defines your game type
+func (*myGame) Type() string { return "myGame" }
+
+// Preload is called before loading any assets from the disk, to allow you to register / queue them
+func (*myGame) Preload() {
+	engo.Files.Add("assets/textures/city.png")
+}
+
+// Setup is called before the main loop starts. It allows you to add entities and systems to your Scene.
+func (*myGame) Setup(world *ecs.World) {
+	engo.SetBackground(color.White)
+
+	world.AddSystem(&engo.RenderSystem{})
+
+	entity := ecs.NewEntity([]string{"RenderSystem"})
+
+	entity.AddComponent(&engo.SpaceComponent{
+		Position: engo.Point{10, 10},
+		Width: 303,
+		Height: 641,
+	})
+
+	texture := engo.Files.Image("city.png")
+	entity.AddComponent(engo.NewRenderComponent(
+		texture,
+		engo.Point{1, 1},
+		"city texture",
+	))
+
+	world.AddEntity(entity)
+}
+
+// Show is called whenever the other Scene becomes inactive, and this one becomes the active one
+func (*myGame) Show() {}
+
+// Hide is called when an other Scene becomes active
+func (*myGame) Hide() {}
+
+func main() {
+	opts := engo.RunOptions{
+		Title: "Hello World",
+		Width: 400,
+		Height: 400,
+	}
+	engo.Run(opts, &myGame{})
+}
