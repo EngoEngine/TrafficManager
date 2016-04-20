901da240df0a65a8d5529771e68481c2e378d594
Etienne Bruines <etienne@bruines.com>
2016-04-20 14:18:43 +0200

    Updated to new ecs

--- systems/citybuilding.go
+++ systems/citybuilding.go
@@ -7,40 +7,40 @@ import (
 	"engo.io/engo"
 )
 
+type MouseTracker struct {
+	ecs.BasicEntity
+	engo.MouseComponent
+}
+
+type City struct {
+	ecs.BasicEntity
+	engo.RenderComponent
+	engo.SpaceComponent
+}
+
 type CityBuildingSystem struct {
 	world *ecs.World
 
-	mouseTracker *ecs.Entity
+	mouseTracker MouseTracker
 }
 
-// Type returns a unique string identifier, usually
-// something like "RenderSystem", "CollisionSystem"...
-func (*CityBuildingSystem) Type() string {
-	return "CityBuildingSystem"
-}
-
-// Priority is used to create the order in which Systems
-// (in the World) are processed
-func (*CityBuildingSystem) Priority() int {
-	return 0
-}
-
-// AddEntity is called whenever an Entity is added to the
-// World, which "requires" this System
-func (*CityBuildingSystem) AddEntity(*ecs.Entity) {}
-
-// RemoveEntity is called whenever an Entity is removed from
-// the World, which "requires" this System
-func (*CityBuildingSystem) RemoveEntity(*ecs.Entity) {}
+// Remove is called whenever an Entity is removed from the scene, and thus from this system
+func (*CityBuildingSystem) Remove(ecs.BasicEntity) {}
 
 // New is the initialisation of the System
 func (cb *CityBuildingSystem) New(w *ecs.World) {
 	cb.world = w
 	fmt.Println("CityBuildingSystem was added to the Scene")
 
-	cb.mouseTracker = ecs.NewEntity("MouseSystem")
-	cb.mouseTracker.AddComponent(&engo.MouseComponent{Track: true})
-	w.AddEntity(cb.mouseTracker)
+	cb.mouseTracker.BasicEntity = ecs.NewBasic()
+	cb.mouseTracker.MouseComponent = engo.MouseComponent{Track: true}
+
+	for _, system := range w.Systems() {
+		switch sys := system.(type) {
+		case *engo.MouseSystem:
+			sys.Add(&cb.mouseTracker.BasicEntity, &cb.mouseTracker.MouseComponent, nil, nil)
+		}
+	}
 }
 
 // Update is ran every frame, with `dt` being the time
@@ -48,30 +48,27 @@ func (cb *CityBuildingSystem) New(w *ecs.World) {
 func (cb *CityBuildingSystem) Update(dt float32) {
 	if engo.Keys.Get(engo.F1).JustPressed() {
 		fmt.Println("The gamer pressed F1")
-		entity := ecs.NewEntity("RenderSystem")
 
-		var (
-			mouse *engo.MouseComponent
-			ok    bool
-		)
+		city := City{BasicEntity: ecs.NewBasic()}
 
-		if mouse, ok = cb.mouseTracker.ComponentFast(mouse).(*engo.MouseComponent); !ok {
-			return
-		}
-
-		entity.AddComponent(&engo.SpaceComponent{
-			Position: engo.Point{mouse.MouseX, mouse.MouseY},
+		city.SpaceComponent = engo.SpaceComponent{
+			Position: engo.Point{cb.mouseTracker.MouseComponent.MouseX, cb.mouseTracker.MouseComponent.MouseY},
 			Width:    30,
 			Height:   64,
-		})
+		}
 
 		texture := engo.Files.Image("city.png")
-		entity.AddComponent(engo.NewRenderComponent(
+		city.RenderComponent = engo.NewRenderComponent(
 			texture,
 			engo.Point{0.1, 0.1},
 			"city texture",
-		))
+		)
 
-		cb.world.AddEntity(entity)
+		for _, system := range cb.world.Systems() {
+			switch sys := system.(type) {
+			case *engo.RenderSystem:
+				sys.Add(&city.BasicEntity, &city.RenderComponent, &city.SpaceComponent)
+			}
+		}
 	}
 }
--- traffic.go
+++ traffic.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"image/color"
+
 	"engo.io/ecs"
 	"engo.io/engo"
 	"github.com/EngoEngine/TrafficManager/systems"
-	"image/color"
 )
 
 const (
@@ -14,18 +15,18 @@ const (
 	ZoomSpeed           = -0.125
 )
 
-type myGame struct{}
+type myScene struct{}
 
 // Type uniquely defines your game type
-func (*myGame) Type() string { return "myGame" }
+func (*myScene) Type() string { return "myGame" }
 
 // Preload is called before loading any assets from the disk, to allow you to register / queue them
-func (*myGame) Preload() {
+func (*myScene) Preload() {
 	engo.Files.Add("assets/textures/city.png")
 }
 
 // Setup is called before the main loop starts. It allows you to add entities and systems to your Scene.
-func (*myGame) Setup(world *ecs.World) {
+func (*myScene) Setup(world *ecs.World) {
 	engo.SetBackground(color.White)
 
 	world.AddSystem(&engo.MouseSystem{})
@@ -41,17 +42,11 @@ func (*myGame) Setup(world *ecs.World) {
 	world.AddSystem(&systems.CityBuildingSystem{})
 }
 
-// Show is called whenever the other Scene becomes inactive, and this one becomes the active one
-func (*myGame) Show() {}
-
-// Hide is called when an other Scene becomes active
-func (*myGame) Hide() {}
-
 func main() {
 	opts := engo.RunOptions{
 		Title:  "Hello World",
 		Width:  400,
 		Height: 400,
 	}
-	engo.Run(opts, &myGame{})
+	engo.Run(opts, &myScene{})
 }
