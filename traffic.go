05802fb87b5a0bea9b38febfa267262423aa28c2
Etienne Bruines <etienne@bruines.com>
2016-04-20 14:18:43 +0200

    Updated to new ecs

--- traffic.go
+++ traffic.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-	"engo.io/engo"
-	"engo.io/ecs"
 	"image/color"
+
+	"engo.io/ecs"
+	"engo.io/engo"
 	"github.com/EngoEngine/TrafficManager/systems"
 )
 
-type myGame struct {}
+type myScene struct{}
 
 // Type uniquely defines your game type
-func (*myGame) Type() string { return "myGame" }
+func (*myScene) Type() string { return "myGame" }
 
 // Preload is called before loading any assets from the disk, to allow you to register / queue them
-func (*myGame) Preload() {
+func (*myScene) Preload() {
 	engo.Files.Add("assets/textures/city.png")
 }
 
 // Setup is called before the main loop starts. It allows you to add entities and systems to your Scene.
-func (*myGame) Setup(world *ecs.World) {
+func (*myScene) Setup(world *ecs.World) {
 	engo.SetBackground(color.White)
 
 	world.AddSystem(&engo.MouseSystem{})
@@ -27,17 +28,11 @@ func (*myGame) Setup(world *ecs.World) {
 	world.AddSystem(&systems.CityBuildingSystem{})
 }
 
-// Show is called whenever the other Scene becomes inactive, and this one becomes the active one
-func (*myGame) Show() {}
-
-// Hide is called when an other Scene becomes active
-func (*myGame) Hide() {}
-
 func main() {
 	opts := engo.RunOptions{
-		Title: "Hello World",
-		Width: 400,
+		Title:  "Hello World",
+		Width:  400,
 		Height: 400,
 	}
-	engo.Run(opts, &myGame{})
+	engo.Run(opts, &myScene{})
 }
