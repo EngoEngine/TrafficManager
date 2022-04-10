a1cf62b5f89652eb4755d0e47ff9520cd0f3db6a
Jumbef <Jumbef@users.noreply.github.com>
2022-04-10 18:24:36 +0200

    Fix (accidental) removal of F1 registering.

--- traffic.go
+++ traffic.go
@@ -38,6 +38,7 @@ func (*myScene) Preload() {
 // Setup is called before the main loop starts. It allows you to add entities and systems to your Scene.
 func (*myScene) Setup(u engo.Updater) {
 	world, _ := u.(*ecs.World)
+	engo.Input.RegisterButton("AddCity", engo.KeyF1)
 	common.SetBackground(color.White)
 
 	world.AddSystem(&common.RenderSystem{})
